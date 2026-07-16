0448ac663f01f52aa3b3760e9d604a43349b6f26
Mahyar2007 <124904095+Mahyar2007@users.noreply.github.com>
2026-07-16 12:20:25 +0330

    Update send.js

--- api/send.js
+++ api/send.js
@@ -2,35 +2,41 @@ import { createClient } from '@supabase/supabase-js';
 
 export default async function handler(req, res) {
 
-  if (req.method !== "POST") {
+  if(req.method !== "POST"){
     return res.status(405).json({
-      error: "Method not allowed"
+      error:"Method not allowed"
     });
   }
 
   const { message, user_id } = req.body;
+
+  if(!message){
+    return res.status(400).json({
+      error:"Message required"
+    });
+  }
 
   const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_KEY
   );
 
-  const { error } = await supabase
+  const { data, error } = await supabase
     .from("messages")
     .insert([
       {
-        telegram_id: user_id,
+        user_id: user_id,
         message: message
       }
     ]);
 
-  if (error) {
+  if(error){
     return res.status(500).json({
-      error: error.message
+      error:error.message
     });
   }
 
   res.status(200).json({
-    success: true
+    success:true
   });
 }
